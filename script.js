f5cf0e440d837ec3cbdaebcae546f9e94b97fb6e
KarolKarnas <karol.karnas@gmail.com>
2022-11-30 12:49:53 +0100

    Add mouseover event to every square
    Mouseover over every square change it color with JS permanently

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ console.log(sketchGridContainer);
 //create 16x 16 grid
 
 for (let i = 1; i <= 16; i++) {
-    const newDiv = document.createElement('div');
-    newDiv.className = 'sketch-tile';
+	const newDiv = document.createElement('div');
+	newDiv.className = 'sketch-tile';
+    newDiv.addEventListener('mouseover', () => {
+    newDiv.style = 'background: deeppink;';
+    })
 
 	sketchGridContainer.appendChild(newDiv);
 }
